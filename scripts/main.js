970428bd98b19d2fa82e2b860546cf4c7355dadf
Sean Tracey <seanmtracey@gmail.com>
2016-09-15 16:14:32 +0100

    Added indicator of time until change

--- scripts/main.js
+++ scripts/main.js
@@ -2,36 +2,73 @@
 
 	'use strict';
 
-	var queryStep = 0;
-	const steps = 3;
-
 	const userInteractionZone = document.querySelector('#demoInteractions');
 	const infoBox = userInteractionZone.querySelector('.info');
 	const startDemoBtn = userInteractionZone.querySelector('#startDemoBtn');
 	const stopDemoBtn = userInteractionZone.querySelector('#stopDemoBtn');
+	const timeUntil = userInteractionZone.querySelector('#timeUntilChange');
 
-	const audioSources = {
-		part1 : '/audio/part1.m4a',
-		article : '/audio/part2.m4a',
-		question1 : '/audio/part3.m4a'
-	};
+	const audioSources = [
+		'/audio/part1.m4a',
+		'/audio/answer_1_a.m4a',
+		'/audio/answer_1_b.m4a',
+		'/audio/answer_1_c.m4a',
+		'/audio/part2.m4a',
+		'/audio/answer_2_a.m4a',
+		'/audio/answer_2_b.m4a',
+		'/audio/continuing.m4a',
+		'/audio/part3.m4a',
+	];
 
+	var queryStep = 0;
+	const steps = audioSources.length;
+	var nextUp = undefined;
+
+	const breathe = 3000;
+	
 	infoBox.textContent = 'Loading Audio Sources';
 	infoBox.dataset.visible = 'true';
 
-	const loadedSources = Object.keys(audioSources).map( (key, idx) => {
+	const loadedSources = audioSources.map( (source, idx) => {
+
+		console.log(source);
 
 		return new Promise( (resolve, reject) => {
 
 			const audioEl = new Audio();
-			audioEl.dataset.name = key;
+			audioEl.dataset.name = source;
 			audioEl.dataset.step = idx;
-			
+
 			audioEl.oncanplay = function(){
 				resolve(this);
 			};
 
-			audioEl.src = audioSources[key];
+			audioEl.ontimeupdate = function(){
+
+				var timeLeft = this.duration - this.currentTime | 0;
+
+				if(timeLeft <= 3){
+					timeUntil.dataset.opaque = "true";
+					timeUntil.textContent = timeLeft;
+				} else if(timeUntil.dataset.opaque !== "false"){
+					timeUntil.dataset.opaque = "false";					
+				}
+
+			}
+
+			audioEl.onended = function(){
+				clearTimeout(nextUp);
+				nextUp = setTimeout(function(){
+
+					if(queryStep < steps){
+						queryStep += 1;
+						document.querySelector('audio[data-step="' + queryStep + '"]').play();
+					}
+	
+				}, breathe);
+			}
+
+			audioEl.src = source;
 			audioEl.setAttribute('hidden', '');
 
 			document.body.appendChild(audioEl);
@@ -66,11 +103,12 @@
 				infoBox.dataset.visible = 'false';
 				startDemoBtn.dataset.visible = 'true';
 				queryStep = 0;
+				clearTimeout(nextUp);
 
 				document.querySelectorAll('audio').forEach(audio => {
 					audio.pause();
 					audio.currentTime = 0;
-				})
+				});
 
 			}, false);
 			
