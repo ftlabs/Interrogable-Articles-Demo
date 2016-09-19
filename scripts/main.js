78b25eb44353812fef867b42ba502f997ac17f62
Sean Tracey <seanmtracey@gmail.com>
2016-09-19 14:23:36 +0100

    Added ability to trigger hidden fine controls

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,24 @@
 
 	'use strict';
 
+	var audioIsUnderway = false;
+
 	const userInteractionZone = document.querySelector('#demoInteractions');
 	const infoBox = userInteractionZone.querySelector('.info');
 	const startDemoBtn = userInteractionZone.querySelector('#startDemoBtn');
 	const stopDemoBtn = userInteractionZone.querySelector('#stopDemoBtn');
+	const fineControlsElement = userInteractionZone.querySelector('#fineGrainControls');
 	const timeUntil = userInteractionZone.querySelector('#timeUntilChange');
+
+
+	const fineControls = { 
+		prev : fineControlsElement.querySelector('#previousBtn'),
+		pause : fineControlsElement.querySelector('#pauseBtn'),
+		resume : fineControlsElement.querySelector('#resumeBtn'),
+		next : fineControlsElement.querySelector('#nextBtn')
+	};
+
+	var shouldShowFineControls = false;
 
 	const audioSources = [
 		'/audio/part1.m4a',
@@ -23,15 +36,34 @@
 	var queryStep = 0;
 	const steps = audioSources.length;
 	var nextUp = undefined;
+	var currentAudio = undefined;
 
 	const breathe = 3000;
 	
+	document.querySelector('header').addEventListener('click', function(){
+
+		if(!shouldShowFineControls){
+			shouldShowFineControls = true;
+			this.dataset.controlsindicator = "true";
+			if(currentAudio !== undefined){
+				if(!currentAudio.paused){
+					fineControlsElement.dataset.visible = "true";
+				}
+			}
+		} else {
+			shouldShowFineControls = false;
+			this.dataset.controlsindicator = "false";
+			fineControlsElement.dataset.visible = "false";
+		}
+
+		console.log(shouldShowFineControls);
+
+	}, false);
+
 	infoBox.textContent = 'Loading Audio Sources';
 	infoBox.dataset.visible = 'true';
 
 	const loadedSources = audioSources.map( (source, idx) => {
-
-		console.log(source);
 
 		return new Promise( (resolve, reject) => {
 
@@ -56,17 +88,36 @@
 
 			}
 
+			audioEl.onplay = function(){
+				fineControls.pause.dataset.visible = "true";
+				fineControls.resume.dataset.visible = "false";
+			};
+
 			audioEl.onended = function(){
 				clearTimeout(nextUp);
-				nextUp = setTimeout(function(){
+				if(queryStep < steps - 1){
+						
+						audioIsUnderway = true;
+						
+						nextUp = setTimeout(function(){
+							queryStep += 1;
+							currentAudio = document.querySelector('audio[data-step="' + queryStep + '"]')
+							currentAudio.play();
+						}, breathe);
 
-					if(queryStep < steps){
-						queryStep += 1;
-						document.querySelector('audio[data-step="' + queryStep + '"]').play();
-					}
+				} else {
+
+					audioIsUnderway = false;
+					stopDemoBtn.dataset.visible = "false";
+					startDemoBtn.dataset.visible = "true";
+
+					infoBox.dataset.visible = 'true';
+					infoBox.dataset.blink = 'true';
+					infoBox.textContent = "";
+
+				}
 	
-				}, breathe);
-			}
+			};
 
 			audioEl.src = source;
 			audioEl.setAttribute('hidden', '');
@@ -87,11 +138,16 @@
 
 				this.dataset.visible = 'false';
 				stopDemoBtn.dataset.visible = 'true';
-				document.querySelector('audio[data-step="0"]').play();
+				currentAudio = document.querySelector('audio[data-step="0"]')
+				currentAudio.play();
 
 				infoBox.textContent = 'Listening for input...';
 				infoBox.dataset.visible = 'true';
 				infoBox.dataset.blink = 'true';
+				// debugger;
+				if(shouldShowFineControls){
+					fineControlsElement.dataset.visible = "true";
+				}
 
 			}, false);
 			startDemoBtn.dataset.visible = 'true';
@@ -103,6 +159,10 @@
 				infoBox.dataset.visible = 'false';
 				startDemoBtn.dataset.visible = 'true';
 				queryStep = 0;
+
+				fineControls.prev.dataset.opaque = "false";
+				fineControls.next.dataset.opaque = "true";
+
 				clearTimeout(nextUp);
 
 				document.querySelectorAll('audio').forEach(audio => {
@@ -110,8 +170,61 @@
 					audio.currentTime = 0;
 				});
 
+				fineControlsElement.dataset.visible = "false";
+
+			}, false);
+
+			fineControls.pause.addEventListener('click', function(){
+				fineControls.pause.dataset.visible = "false";
+				fineControls.resume.dataset.visible = "true";
+				currentAudio.pause();
+			}, false);
+
+			fineControls.resume.addEventListener('click', function(){
+				currentAudio.play();
+				fineControls.resume.dataset.visible = "false";
+				fineControls.pause.dataset.visible = "true";
+			}, false);
+
+			fineControls.prev.addEventListener('click', function(){
+				debugger;
+				if(queryStep > 0){
+
+					currentAudio.pause()
+					currentAudio.currentTime = 0;
+					queryStep -= 1;
+					currentAudio = document.querySelector('audio[data-step="' + queryStep + '"]')
+					currentAudio.play();
+
+					if(queryStep === 0){
+						fineControls.prev.dataset.opaque = "false";
+					} else {
+						fineControls.prev.dataset.opaque = "true";						
+					}
+
+				} else {
+					this.dataset.opaque = "false";
+				}
+
 			}, false);
 			
+			fineControls.next.addEventListener('click', function(){
+				
+				if(queryStep < steps - 1){
+					currentAudio.pause()
+					currentAudio.currentTime = 0;
+					queryStep += 1;
+					currentAudio = document.querySelector('audio[data-step="' + queryStep + '"]')
+					currentAudio.play();
+
+					fineControls.prev.dataset.opaque = "true";
+
+				} else {
+					this.dataset.opaque = "false";
+				}
+
+			}, false);
+
 		})
 		.catch(err => {
 			console.error('An error occurred loading the audio files');
